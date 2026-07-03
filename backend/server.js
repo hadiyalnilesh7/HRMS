59d2ba84a2caad40f00a62f6242682b26cf027ba
Hadiyal Nilesh <hadiyalnilesh6061@gmail.com>
2026-07-03 19:03:34 +0530

    create vercel file

--- backend/server.js
+++ backend/server.js
@@ -105,6 +105,10 @@ app.get("/dashboard", adminAuth, async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is Running on port ${PORT}`);
-});
+if (require.main === module && !process.env.VERCEL) {
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is Running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
